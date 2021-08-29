083448cc278f21c538fa4c9fa71b63d655b9a9c2
itsfuad <fuad.cs22@gmail.com>
2021-08-30 03:16:32 +0600

    auto save

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 const content = document.getElementById("contents");
 const btn = document.getElementById("convert-btn");
 const viewbox = document.getElementById("viewbox");
-const bk = document.getElementById('bk');
-let markdown;
-const cvt = ()=>{
-  console.log("converted");
+let markdown, html, temp;
+
+let typingTimer; //timer identifier
+let doneTypingInterval = 500; //time in ms (5 seconds)
+let myInput = document.getElementById('contents');
+
+
+//on keyup, start the countdown
+myInput.addEventListener('keyup', () => {
+  clearTimeout(typingTimer);
+  if (myInput.value) {
+    typingTimer = setTimeout(updateviewbox, doneTypingInterval);
+  }
+});
+
+//user is "finished typing," do something
+const updateviewbox = ()=> {
+  //do something
   markdown = content.value;
-  let html = marked(markdown);
+  html = marked(markdown);
   viewbox.innerHTML = html;
-  content.value = html;
-  btn.value = "Converted to HTML";
-  btn.disabled = true;
-  bk.disabled = false;
-  return false;
 }
-bk.addEventListener('click', ()=>{
-  content.value = markdown;
-  btn.disabled = false;
-  btn.value = "Convert to HTML";
+
+btn.addEventListener('click', ()=>{
+ 
+  if (btn.value === "Show HTML") {
+    btn.value = "Show Markdown";
+    markdown = content.value;
+    html = marked(markdown);
+    viewbox.innerHTML = html;
+    content.value = html;
+  } 
+  else{
+    btn.value = "Show HTML";
+    content.value = markdown;
+  }
+  
 });
