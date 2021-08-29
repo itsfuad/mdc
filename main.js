3a7e179b4d85da5d7b80e087d4d5d599854ba3ab
itsfuad <fuad.cs22@gmail.com>
2021-08-30 01:42:11 +0600

    done ok

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ const content = document.getElementById("contents");
 const btn = document.getElementById("convert-btn");
 const viewbox = document.getElementById("viewbox");
 
+let markdown;
 const cvt = ()=>{
   console.log("converted");
-  viewbox.innerHTML = marked(content.value);
+  markdown = content.value;
+  let html = marked(markdown);
+  viewbox.innerHTML = html;
+  content.value = html;
   return false;
 }
+document.getElementById('bk').addEventListener('click', ()=>{
+  content.value = markdown;
+});
