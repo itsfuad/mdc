70e56e7b3794d38dd9d9d334ae63467335cc1a59
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-01-30 02:39:19 +0600

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let typingTimer; //timer identifier
 let doneTypingInterval = 500; //time in ms (5 seconds)
 let myInput = document.getElementById('contents');
 
-document.getElementById('version').innerText=`v{process.env.ver}`;
+document.getElementById('version').innerText=process.env.VER;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
